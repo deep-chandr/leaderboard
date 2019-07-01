3edbe60e030f5758bf5059d3b3f326294241fca2
deep <deep.dc216@gmail.com>
2019-07-02 01:36:00 +0530

    modified routes

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import configs from './configs/config.js';
-import main from './public/js/routes/index_page.js';
+import main from './public/js/routes/leaderboard.js';
 import login from './public/js/routes/login.js';
 import './public/css/reset.scss';
-import './public/css/index_page.scss';
+import './public/css/leaderboard.scss';
 import "./public/css/login/login.scss";
 
 
 
 window.onload = function () {
    console.log('bundle file loaded:  ', configs.NODE_ENV);
-   if (window.location.pathname === '/') {
-      
+   if (window.location.pathname !== '/login.html') {
+
       const token = window.localStorage.getItem('xxx-kon-dev-token');
       if (!token) {
          window.location.pathname = "/login.html";
       }
-      main.handle_open_tab(null, 'today');
    }
-   
+   if (window.location.pathname === "/") {
+      window.location.pathname = "/leaderboard.html";
+   }
+   main.handle_open_tab(null, 'today');
+
 };
 
 window.app = {
